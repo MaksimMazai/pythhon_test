e62bef9c1632c6fd5b5b2c50eb5d9875feac847c
maksimmazai <maxi-will@mail.ru>
2022-06-13 12:33:03 +0500

    driver

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-print("hello")
-print("ffwefefwfewefw")
-print("dew")
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import os
+import time
+
+options = webdriver.ChromeOptions()
+binary_yandex_driver_file = 'yandexdriver.exe'
+driver = webdriver.Chrome(binary_yandex_driver_file, options=options)
+driver.get('https://yandex.ru')
+driver.quit()
